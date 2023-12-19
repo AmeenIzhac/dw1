59c9787561184079da39e757b970aacd951dde9c
ameenizhac <ameen.izhac@gmail.com>
2023-12-19 21:39:11 +0000

    dw

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ const App = () => {
 
   // Function to update the state when input changes
   const handleInputChange = (event) => {
-    if (parseInt(event.target.value) == 12) {
-      setText("you can get the reward from x office");
+    if (event.target.value == "229"
+    || event.target.value == "2,2,9"
+    || event.target.value == "2, 2, 9"
+    || event.target.value == "2, 2, 9 "
+    || event.target.value == "922"
+    || event.target.value == "9,2,2"
+    || event.target.value == "9, 2, 2"
+    || event.target.value == "9, 2, 2 "
+    ) {
+      setText("Fantastic! Now just look for your gift in the Maths Office at Upper, behind: \"I stand tall with tales untold, lined with volumes, new and old\"");
     } else {
       setText("how have you still not figured it out?");
     }
